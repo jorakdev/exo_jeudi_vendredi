4b57d7b366b4351f4aae03c83b59bb7cf1bd9e89
jorakdev <jorakmail@gmail.com>
2019-01-04 16:07:29 +0300

    Update exo_20.rb

--- exo_vendredi/exo_20.rb
+++ exo_vendredi/exo_20.rb
@@ -2,10 +2,17 @@ puts "exo 20"
 puts "Combien d'etage voulez vous pour la pyramide"
 print "> "
 nbr_py = gets.chomp().to_i
-nbr = 0
 
-nbr_py.times do
-  nbr = nbr + 1
-  p = "#" * nbr
-  puts p
+if nbr_py.to_i > 25
+  puts "nombre incorrect => fin du program"
+  elsif nbr_py.to_i < 1
+    puts "nombre incorrect => fin du program"
+else
+    nbr = 0
+    nbr_py.times do
+      nbr = nbr + 1
+      p = "#" * nbr
+      puts p
+    end
+  nbr_py +=1
 end
