6294847d75b01f2e650444d2ddcd6749ed3b13af
jorakdev <jorakmail@gmail.com>
2019-01-04 15:47:53 +0300

    Update pyramide.rb

--- exo_vendredi/pyramide.rb
+++ exo_vendredi/pyramide.rb
@@ -1,11 +1,11 @@
-puts "exo 20"
-puts "Combien d'etage voulez vous pour la pyramide"
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 nbr_py = gets.chomp()
 
 for i in (1..nbr_py.to_i)
 
-  #si  'input de l'user est 1 (nbr_py = 1) on entre pas dans cette boucle car la condition n'est pas vérifiée
+  #si  l'input de l'user est 1 (nbr_py = 1) on entre pas dans cette boucle car la condition n'est pas vérifiée
   #on passe directement dans for k in (1..i) ...
   for j in (1..nbr_py.to_i-i)
     print " "
